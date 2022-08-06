cfb85c52d99c457037b0a16c7b27dfc15c03aef0
Bryson Curtis <bcurtis4@uccs.edu>
2022-08-05 23:40:03 -0600

    Adding burst AR

---
+++ Items/BurstAR.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+
+namespace FortniteItems.Items
+{
+	public class BurstAR : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Burst Assault Rifle");
+			Tooltip.SetDefault("Shoots in bursts of 3, only the last bullet consumes ammo\n\"Gotta get that W, 3 shots at a time\"");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+		//a post evil boss rifle intended for early game sustained damage
+		public override void SetDefaults()
+		{
+
+			Item.damage = 20;
+			Item.DamageType = DamageClass.Ranged;
+			Item.width = 40;
+			Item.height = 40;
+			Item.useTime = 3;
+			Item.useAnimation = 9;
+			Item.useStyle = ItemUseStyleID.Shoot;
+			Item.knockBack = 0.2f;
+			Item.value = Item.sellPrice(gold: 2, silver: 50);
+			Item.rare = ItemRarityID.Pink;
+			Item.UseSound = SoundID.Item11;
+			Item.autoReuse = true;
+			Item.shoot = ProjectileID.PurificationPowder;
+			Item.shootSpeed = 70;
+			Item.noMelee = true;
+			Item.useAmmo = AmmoID.Bullet;
+			Item.ArmorPenetration = 25;
+			Item.reuseDelay = 30;
+			Item.consumeAmmoOnLastShotOnly = true;
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.MeteoriteBar, 12);
+			recipe.AddIngredient(ItemID.IllegalGunParts, 1);
+			recipe.AddTile(TileID.Furnaces); 
+			recipe.Register();
+
+		}
+
+		public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(-9f, 0);
+		}
+		public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+		{
+			Vector2 muzzleOffset = Vector2.Normalize(velocity) * 25f;
+
+			if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+			{
+				position += muzzleOffset;
+			}
+
+		}
+
+		public override bool CanConsumeAmmo(Item ammo, Player player)
+		{
+			return Main.rand.NextFloat() >= 0.25f;
+		}
+
+	}
+}
