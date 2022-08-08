327c206f52775ba500b22c7f85a9e4f470595b65
Bryson Curtis <bcurtis4@uccs.edu>
2022-08-07 22:22:31 -0600

    Added Scoped AR and Temp Texture

---
+++ Items/ScopedAR.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+
+namespace FortniteItems.Items
+{
+	public class ScopedAR : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Scoped Assault Rifle");
+			Tooltip.SetDefault("35% chance to not consume ammo\nTurns musket balls into high velocity bullets\n\"Gotta get that W, from a range\"");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+		//a direct upgrade to the assault rifle (or scar)
+		public override void SetDefaults()
+		{
+			Item.damage = 50;
+			Item.DamageType = DamageClass.Ranged;
+			Item.width = 40;
+			Item.height = 40;
+			Item.useTime = 8;
+			Item.useAnimation = 8;
+			Item.useStyle = ItemUseStyleID.Shoot;
+			Item.knockBack = 0.2f;
+			Item.value = Item.sellPrice(gold: 5);
+			Item.rare = ItemRarityID.Yellow; //Post Plantera Crafted with Shroomite
+			Item.UseSound = SoundID.Item48;
+			Item.autoReuse = true;
+			Item.shoot = ProjectileID.PurificationPowder;
+			Item.shootSpeed = 70;
+			Item.noMelee = true;
+			Item.useAmmo = AmmoID.Bullet;
+			Item.ArmorPenetration = 30;
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ModContent.ItemType<SilencedScar>());
+			recipe.AddIngredient(ItemID.ShroomiteBar, 10);
+			recipe.AddIngredient(ItemID.SniperScope, 1);
+			recipe.AddTile(TileID.Autohammer);
+			recipe.Register();
+		}
+
+		public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(-11f, 0);
+		}
+		public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+		{
+			Vector2 muzzleOffset = Vector2.Normalize(velocity) * 25f;
+
+			if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+			{
+				position += muzzleOffset;
+			}
+
+			if (type == ProjectileID.Bullet)
+			{
+				type = ProjectileID.BulletHighVelocity;
+			}
+		}
+
+		public override bool CanConsumeAmmo(Item ammo, Player player)
+		{
+			return Main.rand.NextFloat() >= 0.30f;
+		}
+
+	}
+}
