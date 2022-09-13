101e96265f616fdfbc34d4174dcd88de46d58189
Bryson Curtis <bcurtis4@uccs.edu>
2022-09-13 14:12:47 -0600

    2.1.3

---
+++ Items/DragonsBreathShotgun.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FortniteItems.Items
+{
+	public class DragonsBreathShotgun : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Dragon's Breath Shotgun");
+			Tooltip.SetDefault("Shoots fire instead of musket balls\n\"Fight fire with fire\"");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+		//a basic post skeletron shotgun that hits hard 
+		public override void SetDefaults()
+		{
+
+			Item.damage = 60;
+			Item.DamageType = DamageClass.Ranged;
+			Item.width = 40;
+			Item.height = 40;
+			Item.useTime = 80;
+			Item.useAnimation = 80;
+			Item.useStyle = ItemUseStyleID.Shoot;
+			Item.knockBack = 2;
+			Item.value = Item.sellPrice(gold: 3,silver: 50);
+			Item.rare = ItemRarityID.Orange; //Hellstone post skeletron
+			Item.UseSound = SoundID.Item36;
+			Item.autoReuse = true;
+			Item.shoot = ProjectileID.Flames;
+			Item.shootSpeed = 15;
+			Item.noMelee = true;
+			Item.useAmmo = AmmoID.Bullet;
+		
+		}
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ModContent.ItemType<MakeshiftShotgun>());
+			recipe.AddIngredient(ItemID.Bone, 15);
+			recipe.AddIngredient(ItemID.HellstoneBar, 10);
+			recipe.AddTile(TileID.Hellforge);
+			recipe.Register();
+
+
+		}
+
+		public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(-9f, 0);
+		}
+		public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+		{
+
+			Vector2 muzzleOffset = Vector2.Normalize(velocity) * 25f;
+
+			if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+			{
+				position += muzzleOffset;
+			}
+
+			if (type == ProjectileID.Bullet)
+			{
+				type = ProjectileID.Flames;
+			}
+
+		}
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+
+		{
+			const int NumProjectiles = 4; // The humber of projectiles that this gun will shoot.
+
+			for (int i = 0; i < NumProjectiles; i++)
+			{
+				// Rotate the velocity randomly by 30 degrees at max.
+				Vector2 newVelocity = velocity.RotatedByRandom(MathHelper.ToRadians(10));
+
+				// Decrease velocity randomly for nicer visuals.
+				newVelocity *= 1f - Main.rand.NextFloat(0.5f);
+
+				// Create a projectile.
+				Projectile.NewProjectileDirect(source, position, newVelocity, type, damage, knockback, player.whoAmI);
+			}
+
+			return false; // Return false because we don't want tModLoader to shoot projectile
+		}
+	}
+}
