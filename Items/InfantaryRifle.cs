2026566cbd46afc5bfc3afca9375899214081f9e
Rift90 <56400140+ThePieBaker90@users.noreply.github.com>
2022-12-03 16:42:57 -0700

    Update InfantaryRifle.cs

--- Items/InfantaryRifle.cs
+++ Items/InfantaryRifle.cs
@@ -11,7 +11,7 @@ namespace FortniteItems.Items
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Infantry Rifle");
-			Tooltip.SetDefault("a rifle that is good in many situations\nFires a high velocity bullet instead of musket balls\n\"When a sniper rifle and an assault rifle meet...\"");
+			Tooltip.SetDefault("a longer range rifle\nFires a high velocity bullet instead of musket balls\n\"When a sniper rifle and an assault rifle meet...\"");
 
 			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
 		}
