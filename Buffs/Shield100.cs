bc3d30bb5a0d16cfbe979a09b78c41377418c04f
Bryson Curtis <bcurtis4@uccs.edu>
2022-12-03 16:50:22 -0700

    2.2.5 pre

---
+++ Buffs/Shield100.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace FortniteItems.Buffs
+{
+	public class Shield100 : ModBuff
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("100% Shield");
+			Description.SetDefault("Grants 40 defense.");
+		}
+
+		public override void Update(Player player, ref int buffIndex)
+		{
+			player.statDefense += 40; // Grant a +4 defense boost to the player while the buff is active.
+		}
+	}
+}
---
+++ Items/ChugJug.cs
@@ -0,0 +1,47 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace FortniteItems.Items
+{
+	public class ChugJug : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Chug Jug");
+			Tooltip.SetDefault("Heals the User 400 Health\nGrants \"100% Shield\" buff\nQuick heal grants health and has an instant drinking time but grants no buff\nStandard drinking grants health and buff but has a 2 second drinking time");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 20;
+
+			// Dust that will appear in these colors when the item with ItemUseStyleID.DrinkLiquid is used
+			ItemID.Sets.DrinkParticleColors[Type] = new Color[3] {
+				new Color(47, 254, 198),
+				new Color(59, 254, 237),
+				new Color(59, 254, 254)
+			};
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 20;
+			Item.height = 20;
+			Item.useStyle = ItemUseStyleID.DrinkLiquid;
+			Item.useAnimation = 120;
+			Item.useTime = 120;
+			Item.useTurn = true;
+			Item.UseSound = SoundID.Item3;
+			Item.maxStack = 30;
+			Item.consumable = true;
+			Item.rare = ItemRarityID.Purple; //Post Moonlord
+			Item.value = Item.buyPrice(gold: 1);
+
+			Item.buffType = ModContent.BuffType<Buffs.Shield100>(); // Applies "Shield 100" (40 Defense)
+			Item.buffTime = 28800; // Lasts 8 Minutes
+
+			Item.healLife = 400; // While we change the actual healing value in GetHealLife, Item.healLife still needs to be higher than 0 for the item to be considered a healing item
+			Item.potion = true; // Makes it so this item applies potion sickness on use and allows it to be used with quick heal
+		}
+	}
+}
