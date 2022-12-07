e9e90345121a04e1b83bdb18176f70e42ac846b0
Bryson Curtis <bcurtis4@uccs.edu>
2022-12-06 19:58:11 -0700

    2.2.6.3

--- Projectiles/VGrenade.cs
+++ Projectiles/VGrenade.cs
@@ -24,40 +24,51 @@ namespace FortniteItems.Projectiles
 			Projectile.friendly = true; //Can the projectile deal damage to enemies?
 			Projectile.hostile = false; //Can the projectile deal damage to the player?
 			Projectile.DamageType = DamageClass.Ranged; //Is the projectile shoot by a ranged weapon?
-			Projectile.ignoreWater = true; //Does the projectile's speed be influenced by water?
+			Projectile.ignoreWater = false; //Does the projectile's speed be influenced by water?
 			Projectile.tileCollide = true; //Can the projectile collide with tiles?\
 			Projectile.timeLeft = 180;
+			Projectile.CanExplodeTile(0, 0);
+			Projectile.velocity = new Vector2();
 
-			AIType = ProjectileID.Grenade; //Act exactly like default grenade
+			AIType = ProjectileID.Bomb; //Act exactly like default grenade
+
+
 		}
-
-
+		
 		public override void Kill(int timeLeft)
 		{
 			Vector2 position = Projectile.Center;
+			position.Y -= 60;
+			
 			SoundEngine.PlaySound(new SoundStyle($"{nameof(FortniteItems)}/Assets/Sounds/Items/Guns/GrenadeExplosion")
 			{
-				Volume = 0.6f,
+				Volume = 0.2f,
 				PitchVariance = 0.2f,
 				MaxInstances = 3,
 			});
-			int radius = 40;
-			for (int x = -radius; x <= radius; x++)
-			{
-				for (int y = -radius; y <= radius; y++)
-				{
-					int xPosition = (int)(x + position.X / 64.0f);
-					int yPosition = (int)(y + position.Y / 64.0f);
-
-					if (Math.Sqrt(x * x + y * y) <= radius + 0.5)
-					{
-						if (Main.rand.NextBool(10))
-						{
-							Dust.NewDust(position, 16, 16, DustID.Smoke, 0.0f, 0.0f, 120, new Color(), 1f);
-						}
-					}
-				}
+			if (Main.myPlayer == Projectile.owner)
+            {
+				Lighting.AddLight(Projectile.Center, Color.Orange.ToVector3() * 0.78f);
+				var projectile = Projectile.NewProjectileDirect(Projectile.InheritSource(Projectile), position, Projectile.velocity, ProjectileID.DD2ExplosiveTrapT3Explosion, 100, 10, Main.myPlayer);
 			}
+			
+			//int radius = 40;
+			//for (int x = -radius; x <= radius; x++)
+			//{
+			//	for (int y = -radius; y <= radius; y++)
+			//	{
+			//		int xPosition = (int)(x + position.X / 64.0f);
+			//		int yPosition = (int)(y + position.Y / 64.0f);
+			//
+			//		if (Math.Sqrt(x * x + y * y) <= radius + 0.5)
+			//		{
+			//			if (Main.rand.NextBool(10))
+			//			{
+			//              Dust.NewDust(position, 16, 16, DustID.Smoke, 0f, 0f, 120, new Color(), 1f);
+			//			}
+			//		}
+			//	}
+			//}
 
 		}
 	}
